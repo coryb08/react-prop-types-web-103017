c2995b2b4816304fa6302ad264f5b33c3be67478
github username <github email address>
2018-01-08 17:47:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// Code Goes Here
+import React from "react";
+
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
